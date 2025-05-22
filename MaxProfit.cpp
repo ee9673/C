f98a098bf9283de62f31fe40c337b60ddc6591ba
Codebrewer <2094765848@qq.com>
2025-05-22 19:41:57 +0800

    Update MaxProfit.cpp

--- MaxProfit.cpp
+++ MaxProfit.cpp
@@ -1,20 +1,20 @@
-// ����һ������ prices �����ĵ� i ��Ԫ�� prices[i] ��ʾһ֧������Ʊ�� i ��ļ۸�
-// ��ֻ��ѡ�� ĳһ�� ������ֻ��Ʊ����ѡ���� δ����ĳһ����ͬ������ �����ù�Ʊ�����һ���㷨�����������ܻ�ȡ���������
-// ��������Դ���ʽ����л�ȡ�������������㲻�ܻ�ȡ�κ����󣬷��� 0 ��
+// 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+// 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+// 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
 #include <stdio.h>
 int MaxProfit(int* prices,int size)
 {
-    // ÿ�ζ�������칺��,����֮����������,����֮ǰ������������滻
+    // 每次都假设今天购买,计算之后的最大利润,若比之前最大利润更大就替换
     int maxP = 0;
     int min = __INT_MAX__;
     for (int i = 0; i < size;i++)
     {
-        // �����ǰ��ͼ۵��ڸ���۸�,���滻��ͼ�
-        if(min < prices[i])
+        // 如果当前最低价低于该天价格,则替换最低价
+        if(min > prices[i])
         {
             min = prices[i];
         }else{
-            // �����ǰ���������ڽ���۸��ȥ��ͼ�,���滻��ǰ�������
+            // 如果当前最大利润低于今天价格减去最低价,则替换当前最大利润
             if(maxP< (prices[i]-min))
             {
                 maxP = prices[i]-min;
